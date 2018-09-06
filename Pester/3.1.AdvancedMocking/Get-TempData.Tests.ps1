31968887e0f856448e54f7a90ed05ae0d889bf3f
Brandon Lundt <brandon.lundt@plexus.com>
2018-09-05 20:33:26 -0500

    Added second mock for get-childitem

--- Pester/3.1.AdvancedMocking/Get-TempData.Tests.ps1
+++ Pester/3.1.AdvancedMocking/Get-TempData.Tests.ps1
@@ -25,6 +25,11 @@ Describe -name "Get-TempData" {
 			}
 			return $Output
 		}
+
+        Mock -CommandName Get-ChildItem -ParameterFilter { $Path -and $Include } -ModuleName Get-Tempdata -MockWith {
+			
+			return New-MockObject -Type System.IO.DirectoryInfo
+		}
 	
 		It -Name "Function returns directory object" -test {
 			$Result = Get-TempData
@@ -32,13 +37,20 @@ Describe -name "Get-TempData" {
 			$Result.Count | Should -BeGreaterThan 2
 		}
 		$Result = Get-TempData -Filter
-		It -Name "Assert we are mocking Get-ChildItem" -test {
+		It -Name "Assert we are mocking Get-ChildItem with Filter and Path parameters" -test {
 			Assert-MockCalled -CommandName Get-ChildItem -ModuleName Get-TempData
 		}
 		It -name "Confirm we are getting expected hashtable properties" -test {
 			$Result | Should -Not -BeNullOrEmpty -Because "We are testing things, we got output!"
 			$Result.Count | Should -Be 1
 			$Result | Should -BeOfType Hashtable -Because "We are expecting this to be our mocked output."
+		}
+		$Result = Get-TempData -Include
+		It -Name "Assert we are mocking Get-ChildItem with Include and Path parameters" -test {
+			Assert-MockCalled -CommandName Get-ChildItem -ModuleName "Get-TempData" -ParameterFilter {$Path -and $Include}
+		}
+		It -name "Confirm we are getting expected System.IO.DirectoryInfo properties" -test {
+			$Result | Should -BeOfType System.IO.DirectoryInfo -Because "We are expecting this to be our mocked output."
 		}
 	}#Context
 }
--- Pester/3.1.AdvancedMocking/Get-TempData.psm1
+++ Pester/3.1.AdvancedMocking/Get-TempData.psm1
@@ -1,13 +1,20 @@
 ﻿function Get-TempData {
 	param(
-		[switch]$Filter
+		[switch]$Filter,
+        [switch]$Include
 	)
 	process {
 		if ( $Filter) {
 			$Results = Get-ChildItem -Path $env:TEMP -Filter *.log
 		}
 		else {
-			$Results = Get-ChildItem -Path $env:TEMP
+            if( $Include){
+                Write-Information -MessageData "Guys, I have stuff"
+                $Results = Get-ChildItem -Path $env:TEMP -Include *.rar
+            }
+            else{
+			    $Results = Get-ChildItem -Path $env:TEMP
+            }
 		}
 		Write-Output $Results
 	}
