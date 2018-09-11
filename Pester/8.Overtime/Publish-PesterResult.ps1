d90d11d47fd89b36d5e117e2489f80ef595afed5
Brandon Lundt <brandon.lundt@plexus.com>
2018-09-10 20:59:10 -0500

    Updated colors to 'practical'

--- Pester/8.Overtime/Publish-PesterResult.ps1
+++ Pester/8.Overtime/Publish-PesterResult.ps1
@@ -11,59 +11,57 @@ $Dashboard = {
 	New-UDDashboard -Title "Pester Results" -NavBarColor '#FF1c1c1c' -NavBarFontColor "#FF55b3ff" -BackgroundColor "#FF333333" -FontColor "#FFFFFF" -Content {
         New-UDRow {
             New-UDColumn -MediumSize 6 -Content{
-			    New-UDChart -Title "Overall Failure Percentage" -Type Bar @DefaultDisplay -Endpoint {
+			    New-UDChart -Title "Overall Failure Percentage" -FontColor Black -Type Bar @DefaultDisplay -Endpoint {
 				    [PSCustomObject]@{ 
 					    Failed = $pesterOut.FailedCount
                             Passed = $PesterOut.PassedCount
 						    Name = "Total"
 					
 				    } | Out-UDChartData -LabelProperty "Name" -Dataset @(
-                       New-UdChartDataset -DataProperty "Failed" -Label "Failed Count" -BackgroundColor "#80962F23" -HoverBackgroundColor "#80962F23"
-                       New-UdChartDataset -DataProperty "Passed" -Label "Passed Count" -BackgroundColor "#8014558C" -HoverBackgroundColor "#8014558C"
+                       New-UdChartDataset -DataProperty "Failed" -Label "Failed Count" -BackgroundColor "#80962F23" -HoverBackgroundColor "#88962F23"
+                       New-UdChartDataset -DataProperty "Passed" -Label "Passed Count" -BackgroundColor "#8014558C" -HoverBackgroundColor "#8814558C"
                    )#Dataset array
 			    }#New-UDChart
                 
-					}#New-UDColumn
-					New-UDColumn -MediumSize 6 -Content {
-						New-UDChart -Title "Failures by Context" -Type Doughnut @DefaultDisplay -Endpoint { 
-							$Context = $InputObject.TestResult | Select-Object -ExpandProperty Context -Unique
-							$Data = @()
-							foreach ( $Item in $Context) {
-								$Fail_Data = $InputObject.TestResult | Where-Object {($_.Context -eq $Item) -and ($_.Result -ne "Passed")}
-								$Pass_Data = $InputObject.TestResult | Where-Object {($_.Context -eq $Item) -and ($_.Result -eq "Passed")}
-								if ( $Pass_Data.Count ) {
-									$Data += [PSCustomObject]@{ Context = $Item; Failures = $Fail_Data.Count; Passed = $Pass_Data.Count }
-								}
-								else {
-									$Data += [PSCustomObject]@{ Context = $Item; Failures = $Fail_Data.Count; Passed = 4 }
-								}
-							}
-							$Data | Out-UDChartData -LabelProperty "Context" -Dataset @(
-								New-UdChartDataset -DataProperty "Failures" -Label "Failed Count" -BackgroundColor "#80941F23" -HoverBackgroundColor "#80962F23"
-								New-UdChartDataset -DataProperty "Passed" -Label "Passed Count" -BackgroundColor "#FF332233" -HoverBackgroundColor "#FF334433"
-							)#Dataset array
-						} #New-UDChart
-					}#New-UDColumn
-				}#New-UDRow
-				New-UDRow -EndPoint {
-					$Describes = $InputObject.TestResult | Select-Object -Unique -ExpandProperty Describe
-					Foreach ( $Table in $Describes) {
-						New-UDColumn -LargeSize 6 -Endpoint {
-							New-UDTable -Title "$Table  Failures" -Headers @("Name", "Context", "Result") -BackgroundColor "#80941F23" -FontColor White -Endpoint {
-								$InputObject.TestResult | Where-Object Describe -eq $Table | Where-Object Result -ne "Passed" | Out-UDTableData -Property @("Name", "Context", "Result")
-							}
-						}#New-UDColumn
-						New-UDColumn -LargeSize 6 -Endpoint {
-							New-UDTable -Title "$Table  Passed" -Headers @("Name", "Context", "Result") -BackgroundColor "#FF334433" -FontColor White -Endpoint {
-								$InputObject.TestResult | Where-Object Describe -eq $Table | Where-Object Result -eq "Passed" | Out-UDTableData -Property @("Name", "Context", "Result")
-							}
-						}#New-UDCOlumn
-					}#foreach
-				}#New-UDRow
-			}#New-UDDashboard
-		}#Dashboard
-
-		$ResultsDashboard = Get-UDDashboard
+            }#New-UDColumn
+            New-UDColumn -MediumSize 6 -Content {
+			    New-UDChart -Title "Failures by Context" -FontColor Black -Type Doughnut @DefaultDisplay -Endpoint { 
+                    $Context = $PesterOut.TestResult | Select-Object -ExpandProperty Context -Unique
+                    $Data = @()
+                    foreach( $Item in $Context){
+                        $Fail_Data = $PesterOut.TestResult | Where-Object {($_.Context -eq $Item) -and ($_.Result -ne "Passed")}
+                        $Pass_Data = $PesterOut.TestResult | Where-Object {($_.Context -eq $Item) -and ($_.Result -eq "Passed")}
+                        if( $Pass_Data.Count ){
+                            $Data += [PSCustomObject]@{ Context = $Item; Failures = $Fail_Data.Count; Passed = $Pass_Data.Count }
+                        }
+                        else{
+                        $Data += [PSCustomObject]@{ Context = $Item; Failures = $Fail_Data.Count; Passed = 4 }
+                        }
+                    }
+                    $Data | Out-UDChartData -LabelProperty "Context" -Dataset @(
+                       New-UdChartDataset -DataProperty "Failures" -Label "Failed Count" -BackgroundColor "#80941F23" -HoverBackgroundColor "#F0962F23"
+                       New-UdChartDataset -DataProperty "Passed" -Label "Passed Count" -BackgroundColor "#AA332233" -HoverBackgroundColor "#F0334433"
+                   )#Dataset array
+			    } #New-UDChart
+            }#New-UDColumn
+		}#New-UDRow
+        New-UDRow -EndPoint {
+            $Describes = $PesterOut.TestResult | Select-Object -Unique -ExpandProperty Describe
+            Foreach( $Table in $Describes){
+                New-UDColumn -LargeSize 6 -Endpoint {
+                New-UDTable -Title "$Table -- Failures" -Headers @("Name", "Context","Result") -BackgroundColor "#80941F23" -FontColor "#FFFFFF" -Endpoint {
+                    $PesterOut.TestResult | Where-Object Describe -eq $Table | Where-Object Result -ne "Passed" | Out-UDTableData -Property @("Name","Context","Result")
+                }
+                }#New-UDColumn
+                New-UDColumn -LargeSize 6 -Endpoint {
+                New-UDTable -Title "$Table -- Passed" -Headers @("Name", "Context","Result") -BackgroundColor "#FF334433" -FontColor "#FFFFFF" -Endpoint {
+                    $PesterOut.TestResult | Where-Object Describe -eq $Table | Where-Object Result -eq "Passed" | Out-UDTableData -Property @("Name","Context","Result")
+                }
+                }#New-UDCOlumn
+            }#foreach
+        }#New-UDRow
+	}#New-UDDashboard
+}#Dashboard
 
 		if ($ResultsDashboard) {
 			$ResultsDashboard | Stop-UDDashboard
