4d25ac1a6cc6f7740c1fd4f802f5075638f934f1
Brandon Lundt <brandon.lundt@plexus.com>
2018-12-21 14:31:21 -0600

    Stuff

--- Module/3.Git/build.ps1
+++ Module/3.Git/build.ps1
@@ -1,8 +1,11 @@
-task -name "Git" -preaction { Write-Host "Checking Git Status"} -precondition {}
+task -name "Git" -preaction { Write-Host "Checking Git Status"} -precondition { git status -s} -action{
+    Write-Error -Message "Git status: $((git status -s) -join ', ')"
+}
+
 task -name "PreBuild Checklist" -depends "Git" 
 
 task -name "Import" -depends "WriteOutput" -preaction { Write-Host "Hello from Import"} -action { 
     Get-ChildItem | Write-Verbose
 }
 
-task -name "default" -depends "Import"
+task -name "default" -depends "PreBuild Checklist","Import"
