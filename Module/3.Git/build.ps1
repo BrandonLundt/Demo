c33614a9b3b08b901371611fe22b3a3cdc87bb5b
Tom.Dietrich <brandon.lundt@plexus.com>
2019-01-06 21:28:31 -0600

    Renamed build tasks

--- Module/3.Git/build.ps1
+++ Module/3.Git/build.ps1
@@ -1,11 +1,23 @@
 task -name "Git" -preaction { Write-Host "Checking Git Status"} -precondition { git status -s} -action{
     Write-Error -Message "Git status: $((git status -s) -join ', ')"
 }
-
-task -name "PreBuild Checklist" -depends "Git" 
-
-task -name "Import" -depends "WriteOutput" -preaction { Write-Host "Hello from Import"} -action { 
-    Get-ChildItem | Write-Verbose
+task -name "Unload" -description "Unloads MonoModule from the current session" -action { 
+    Remove-Module MonoModule
 }
 
-task -name "default" -depends "PreBuild Checklist","Import"
+task -name "PreBuild Checklist" -depends "Git","Unload"
+
+task -name "CopyToModuleFolder" -action { 
+    Write-Host "Actual Action"
+    $ModuleFolder = "C:\Users\brandon.lundt\Documents\PowerShell\Modules\MonoModule\0.2"
+    if( Test-Path $ModuleFolder ){
+        Get-Item *.ps[dm]1 | Copy-Item -Destination $ModuleFolder
+    }
+}
+
+task -name "Import" -depends "WriteOutput" -preaction { Write-Host "Hello from Import"} -action { 
+    Get-Module -ListAvailable Mon* | Import-Module
+}
+task -name "Build" -depends "PreBuild Checklist","CopyToModuleFolder","Import"
+
+task -name "default" -depends "PreBuild Checklist","Build"
